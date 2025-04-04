991bc2dc3eff40eba1f8abd1b91fe4f1e764438d
StarDuster01 <59341459+StarDuster01@users.noreply.github.com>
2025-04-04 16:46:24 -0400

    initial commit

---
+++ src/client/java/starduster/circuitmod/CircuitmodDataGenerator.java
@@ -0,0 +1,11 @@
+package starduster.circuitmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CircuitmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
