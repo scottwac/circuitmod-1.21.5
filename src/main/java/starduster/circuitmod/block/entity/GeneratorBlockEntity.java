5ab26e30aa8c0c31fcb5786de3acebaa5d689c5d
GreatCow <47872272+GreatCow@users.noreply.github.com>
2025-07-21 16:23:24 -0400

    Fuel Generator sounds and particles

--- src/main/java/starduster/circuitmod/block/entity/GeneratorBlockEntity.java
+++ src/main/java/starduster/circuitmod/block/entity/GeneratorBlockEntity.java
@@ -17,6 +17,7 @@ import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
@@ -25,10 +26,12 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import starduster.circuitmod.block.machines.Generator;
+import starduster.circuitmod.block.machines.QuarryBlock;
 import starduster.circuitmod.power.EnergyNetwork;
 import starduster.circuitmod.power.IEnergyProducer;
 import starduster.circuitmod.power.IPowerConnectable;
 import starduster.circuitmod.screen.GeneratorScreenHandler;
+import starduster.circuitmod.sound.ModSounds;
 import starduster.circuitmod.util.ImplementedInventory;
 
 public class GeneratorBlockEntity extends BlockEntity implements NamedScreenHandlerFactory, ImplementedInventory, IEnergyProducer, IPowerConnectable {
@@ -106,8 +109,9 @@ public class GeneratorBlockEntity extends BlockEntity implements NamedScreenHand
         maxBurnTime = nbt.getInt("generator.max_burn_time").orElse(0);
         powerProduction = nbt.getInt("generator.power_production").orElse(0);
     }
-    
-    public static void tick(World world, BlockPos pos, BlockState state, GeneratorBlockEntity entity) {
+
+    private int soundClock = 0;
+    public void tick(World world, BlockPos pos, BlockState state, GeneratorBlockEntity entity) {
         boolean burningBefore = entity.isBurning();
         
         // Decrease burn time
@@ -133,6 +137,19 @@ public class GeneratorBlockEntity extends BlockEntity implements NamedScreenHand
         }
         
         entity.markDirty();
+
+        if(isBurning()) {
+            if(soundClock <= 0){
+                soundClock = 60;
+            }
+            if(soundClock == 60) {
+                world.playSound(null, pos, ModSounds.BURNING_FUEL_GENERATOR, SoundCategory.BLOCKS, 0.5F, 1F);
+            }
+            soundClock = soundClock - 1;
+        }
+        if(!isBurning()) {
+            soundClock = 60;
+        }
     }
     
     public boolean isBurning() {
